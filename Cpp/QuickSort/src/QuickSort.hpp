da3c9e22b7587ab1df395db26a31af746b1672aa
dohai90 <mr.dohai.90@gmail.com>
2018-06-03 15:00:14 +0900

    Add Quick sort class

---
+++ Cpp/QuickSort/src/QuickSort.hpp
@@ -0,0 +1,58 @@
+/*
+ * QuickSort.hpp
+ *
+ *  Created on: 3 Jun 2018
+ *      Author: dohai90
+ */
+
+#ifndef QUICKSORT_HPP_
+#define QUICKSORT_HPP_
+
+#include <vector>
+
+template <typename E>
+class Comparator {
+public:
+	bool operator()(const E& e1, const E& e2) const {return e1 < e2;}
+};
+
+template <typename E, typename C>
+class Quick {
+public:
+	static void sort(std::vector<E>& S, const C& less);
+protected:
+	static void sort(std::vector<E>& S, unsigned int lo, unsigned int hi, const C& less);
+	static unsigned int partition(std::vector<E>& S, unsigned int lo, unsigned int hi, const C& less);
+};
+
+template <typename E, typename C>
+void Quick<E, C>::sort(std::vector<E>& S, const C& less){
+	sort(S, 0, S.size() - 1, less);
+}
+
+template <typename E, typename C>
+void Quick<E, C>::sort(std::vector<E>& S, unsigned int lo, unsigned int hi, const C& less){
+	if(hi <= lo) return;
+	unsigned int j = partition(S, lo, hi, less);
+
+	sort(S, lo, j-1, less);
+	sort(S, j+1, hi, less);
+}
+
+template <typename E, typename C>
+unsigned int Quick<E, C>::partition(std::vector<E>& S, unsigned int lo, unsigned int hi, const C& less){
+	unsigned int i = lo;
+	unsigned int j = hi + 1;
+	E v = S[lo];
+	while(true){
+		while(less(S[++i], v)) if(i == hi) break;
+		while(less(v, S[--j])) if(j == lo) break;
+		if(i >= j) break;
+		std::swap(S[i], S[j]);
+	}
+	std::swap(S[lo], S[j]);
+	return j;
+}
+
+
+#endif /* QUICKSORT_HPP_ */
